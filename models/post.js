549a6887428452b0b314398aaa6b212bdf432958
argie <argeline0729@yahoo.com>
2022-04-26 17:12:43 -0400

    still

--- models/post.js
+++ models/post.js
@@ -18,7 +18,7 @@ const postSchema = new mongoose.Schema({
 // mongoose.model(<mongodb collection name>, our schema)
 const post = mongoose.model('Post', postSchema);
 
-module.exports = post;
+module.exports = post
 
 // we will access the array data through our 'database'
 // without our module.exports we would not be able to access data from this file
