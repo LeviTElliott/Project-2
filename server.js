69b3b06e825cc159e5267fa5ed511e5d5ab8a4ea
Levi Elliott <levihackworth@yahoo.com>
2022-04-26 09:21:13 -0500

    folders and directories commited

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-PORT = 4100
+// import express
+const express = require('express');
+const methodOverride = require('method-override')
+// const productController = require('./controllers/products_controller')
+const controllers = require('./controllers')
+// create instance
+const app = express();
+
+
+// db connection
+require('./config/db.connection')
+
+// configure the app settings (used by app.listen)
+const PORT = 4100;
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
