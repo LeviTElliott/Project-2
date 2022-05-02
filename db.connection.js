632cca0141ec664d8e991e01968bca96a89620e1
Levi Elliott <levihackworth@yahoo.com>
2022-05-02 14:28:02 -0500

    databses work

---
+++ db.connection.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const connectionStr = process.env.MONGODB_URL
+mongoose.connect(connectionStr);
+mongoose.connection.on('connected' , () => {
+  console.log('connected')
+});
+mongoose.connection.on ('error', (error) => {
+  console.log(`Error! ${error}`);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected')
+});
